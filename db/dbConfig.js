efb693e6d7224764bba51ff77079ba5658683373
hayleuser <hailyemisganew@gmail.com>
2024-12-13 13:18:17 -0900

    initial commit

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,7 @@
 const mysql2 = require("mysql2");
 const dbConnection = mysql2.createPool({
   user: "evangadi-admin",
-  database: "evangadi forum",
+  database: "evangadi-forum",
   host: "localhost",
   password: "123456",
   connectionLimit: 10,
