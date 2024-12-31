1a6d92e1acdbdca00500fff114c6edcc2c9de50f
syimer1627 <seifuy@hotmail.com>
2024-12-31 00:02:54 -0500

    Update vite config for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the 'dist' folder
+    sourcemap: true // Optional: Generate source maps for debugging in production (optional for production)
+  }
 })
+
